e37f9a4aad7ba4e6ed2d520cb7682e361c4824e0
mohamedsaid-sd <sudanit2015@gmail.com>
2024-01-18 20:53:24 +0200

    add to git

---
+++ lang/ar.php
@@ -0,0 +1,14 @@
+<?php
+
+define('_ADD_CAR' , 'اضافة سيارة');
+define('_HELLO' , 'مرحبا');
+define('_LANG' , 'اللغة');
+define('_MAIN' , 'القائمة');
+define('_WEB_SITE_NAME' , 'سوق التعديين');
+
+// القائمة الرئيسية
+define('_HOME' , 'الرئيسية');
+define('_STORE' , 'المتجر');
+
+
+?>
---
+++ lang/en.php
@@ -0,0 +1,13 @@
+<?php
+
+define('_ADD_CAR' , 'add car');
+define('_HELLO' , 'Hello');
+define('_LANG' , 'Language');
+define('_MAIN' , 'Mainlist');
+define('_WEB_SITE_NAME' , 'Ta3deen');
+
+// the mainlist
+define('_HOME' , 'Home');
+define('_STORE' , 'Store');
+
+?>
